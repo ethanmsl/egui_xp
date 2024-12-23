b722bed39bac69c2a646f1e76eb0e02e6f04f959
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2024-12-23 12:10:43 +0100

    chore: style/lint

--- src/bin/simple.rs
+++ src/bin/simple.rs
@@ -8,7 +8,6 @@ use egui_xp::Result as MyResult; // NOTE: this is mine.
 use egui_xp::active_global_default_tracing_subscriber;
 
 fn main() -> MyResult<()> {
-        println!("Hello, world!");
         let _log_writer_guard = active_global_default_tracing_subscriber()?;
 
         let native_options = eframe::NativeOptions::default();
@@ -66,7 +65,7 @@ struct AppStruct {
 }
 
 impl AppStruct {
-        fn new(cc: &eframe::CreationContext<'_>) -> Self {
+        fn new(_cc: &eframe::CreationContext<'_>) -> Self {
                 // Customize egui here with cc.egui_ctx.set_fonts and cc.egui_ctx.set_visuals.
                 // Restore app state using cc.storage (requires the "persistence" feature).
                 // Use the cc.gl (a glow::Context) to create graphics shaders and buffers that you can use
