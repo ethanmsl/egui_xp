aee8fccac8c707e629ec36ab10fc0b0ac54f8e69
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2024-12-29 21:42:54 -0500

    chore: dual import (main, lib) confused clippy

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,5 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
-// use egui_xp::TemplateApp;
-mod app;
-mod support;
-
 use egui_xp::{Result, TemplateApp};
 
 // When compiling natively:
